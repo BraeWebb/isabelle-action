be5401f4f0e5be4ff8348eface2c99bb7564a843
pandaman64 <kointosudesuyo@infoseek.jp>
2020-09-19 20:50:12 +0900

    debug messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 set -eo pipefail
 
 echo "$*"
+echo "$HEAP_CACHE_DIR"
+
+ls -al .
 
 # Copy Heap Images from Cache to Local
 # This command uses `isabelle env` for populating a new shell with Isabelle environment variables,
@@ -10,7 +13,14 @@ echo "$*"
 # begin substitued by the outer shell (that does not have the environment variables).
 /Isabelle2020/bin/isabelle env bash << 'EOC'
 if [ -n "$HEAP_CACHE_DIR" -a -d "$HEAP_CACHE_DIR" ]; then
+  ls -al "$HEAP_CACHE_DIR"
   cp -r "$HEAP_CACHE_DIR" "$ISABELLE_HEAPS"
+fi
+EOC
+
+/Isabelle2020/bin/isabelle env bash << 'EOC'
+if [ -n "$HEAP_CACHE_DIR" -a -d "$ISABELLE_HEAPS" ]; then
+  ls -al "$ISABELLE_HEAPS"
 fi
 EOC
 
@@ -21,5 +31,6 @@ sh -c "/Isabelle2020/bin/isabelle $*"
 /Isabelle2020/bin/isabelle env bash << 'EOC'
 if [ -n "$HEAP_CACHE_DIR" -a -d "$ISABELLE_HEAPS" ]; then
   cp -r "$ISABELLE_HEAPS" "$HEAP_CACHE_DIR"
+  ls -al "$HEAP_CACHE_DIR"
 fi
 EOC
