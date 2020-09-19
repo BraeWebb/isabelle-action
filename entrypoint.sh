5ec77f4c70731b1d7b7f24e07975597134e4d9b6
pandaman64 <kointosudesuyo@infoseek.jp>
2020-09-19 15:18:10 +0900

    copy heap

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
+echo "$*"
+
+# Run Isabelle
 sh -c "/Isabelle2020/bin/isabelle $*"
+
+# Copy Heap
+if [ -d /root/.isabelle/Isabelle2020/heaps ]; then
+   env "$(/Isabelle2020/bin/isabelle getenv ISABELLE_HEAPS)" sh -c 'cp -r "$ISABELLE_HEAPS" "$GITHUB_WORKSPACE/heaps"' 
+fi
